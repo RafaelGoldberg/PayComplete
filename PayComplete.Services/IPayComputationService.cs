08d923c3619cd4363d6aa98a6e18e726b5db1a2c
Rafael <Rafael@TheBigMachine>
2020-10-27 15:10:54 -0400

    Payment interface and implementation

---
+++ PayComplete.Services/IPayComputationService.cs
@@ -0,0 +1,26 @@
+﻿using PayComplete.Entity;
+using System;
+using System.Collections.Generic;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PayComplete.Services
+{
+    public interface IPayComputationService
+    {
+        Task CreateAsync(PaymentRecord paymentRecord);
+        PaymentRecord GetById(int id);
+        IEnumerable<PaymentRecord> GetAll();
+        IEnumerable<SelectListItem> GetAllTaxYear();
+        decimal OverTimeHours(decimal hoursWorked, decimal contactualHours);
+        decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate);
+        decimal OvertimeRate(decimal hourlyRate);
+        decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours);
+        decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings);
+        decimal TotalDeduction(decimal tax, decimal ssc, decimal studentLoanRepayment, decimal unionFee); // ssc = Socail Security Contribution 
+        decimal NetPay(decimal totalEarnings, decimal totalDeductions);
+
+    }
+}
---
+++ PayComplete.Services/Implementation/PayComputationService.cs
@@ -0,0 +1,79 @@
+﻿using PayComplete.Entity;
+using PayComplete.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace PayComplete.Services.Implementation
+{
+    public class PayComputationService : IPayComputationService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        private decimal contractualEarnings;
+        private decimal overtimeHours;
+
+        public PayComputationService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public decimal ContractualEarnings(decimal contractualHours, decimal hoursWorked, decimal hourlyRate)
+        {
+            if (hoursWorked < contractualHours)
+            {
+                contractualEarnings = hoursWorked * hourlyRate;
+            }
+            else
+            {
+                contractualEarnings = contractualHours * hourlyRate;
+            }
+            return contractualHours;
+        }
+
+        public async Task CreateAsync(PaymentRecord paymentRecord)
+        {
+            await _dbContext.PaymentRecords.AddAsync(paymentRecord);
+            await _dbContext.SaveChangesAsync();
+        }
+
+        public IEnumerable<PaymentRecord> GetAll() => _dbContext.PaymentRecords.OrderBy(p => p.EmployeeId);
+
+        public IEnumerable<SelectListItem> GetAllTaxYear()
+        {
+            var allTaxYears = _dbContext.TaxYears.Select(taxYears => new SelectListItem
+            {
+                Text = taxYears.YearOfTax,
+                Value = taxYears.Id.ToString()
+            });
+            return allTaxYears;
+        }
+
+        public PaymentRecord GetById(int id) => _dbContext.PaymentRecords.Where(pay => pay.Id == id).FirstOrDefault();
+
+        public decimal NetPay(decimal totalEarnings, decimal totalDeductions) => totalEarnings - totalDeductions;
+
+        public decimal OvertimeEarnings(decimal overtimeRate, decimal overtimeHours) => overtimeHours * overtimeRate;
+
+        public decimal OverTimeHours(decimal hoursWorked, decimal contactualHours)
+        {
+            if (hoursWorked <= contactualHours)
+            {
+                overtimeHours = 0.00m;
+            }
+            else if (hoursWorked > contactualHours)
+            {
+                overtimeHours = hoursWorked - contactualHours;
+            }
+            return overtimeHours;
+        }
+
+        public decimal OvertimeRate(decimal hourlyRate) => hourlyRate * 1.5m;
+
+        public decimal TotalDeduction(decimal tax, decimal ssc, decimal studentLoanRepayment, decimal unionFee) 
+            => tax + ssc + studentLoanRepayment + unionFee;
+
+        public decimal TotalEarnings(decimal overtimeEarnings, decimal contractualEarnings) => overtimeHours + contractualEarnings;
+    }
+}
